a1bbf5e3fc9858b733690874372fa34f73109090
lazzereis <111388673+lazzereis@users.noreply.github.com>
2024-10-01 22:53:11 -0300

    Update tb end.sql

--- tb end.sql
+++ tb end.sql
@@ -1,8 +1,8 @@
 create table tb_endereço(
 id_endereço primary key not null,
-end_CEP VARCHAR(45),
-end_rua varchar(45),
-end_bairro VARCHAR(45)
+end_CEP VARCHAR(45) not null,
+end_rua varchar(45) not null,
+end_bairro VARCHAR(45) not null
 
 
 
